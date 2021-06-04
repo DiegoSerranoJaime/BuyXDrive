12449d2f88208bbaafe0fd628ec51b913cde526f
DESKTOP-RA6IOE8\Usuario <diegoserranojaime2001@gmail.com>
2021-06-04 15:56:05 +0200

    [BXD-49] [WEB] [API] Agregar y modificar vehiculos

--- API/app/model/AdminVehiclesModel.js
+++ API/app/model/AdminVehiclesModel.js
@@ -2,7 +2,22 @@
 
 const sql = require('./db');
 
-let AdminVehicles = function() {}
+let AdminVehicles = function(vehicle) {
+    this.id = vehicle.id;
+    this.model_id = vehicle.modelId;
+    this.type = vehicle.type;
+    this.cv = vehicle.cv;
+    this.traction = vehicle.traction;
+    this.transmission = vehicle.transmission;
+    this.status = vehicle.km > 0 ? 'Seminuevo' : 'Nuevo';
+    this.km = vehicle.km;
+    this.fuel = vehicle.fuel;
+    this.consumption = vehicle.consumption;
+    this.doors = vehicle.doors;
+    this.weight = vehicle.weight;
+    this.seating = vehicle.seating;
+    this.inner_materials = vehicle.innerMaterials;
+}
 
 AdminVehicles.getAllVehicles = function(result) {
     
@@ -101,6 +116,34 @@ AdminVehicles.delete = function(id, result) {
     let query = `DELETE FROM products WHERE id = ?`;
 
     sql.query(query, id ,(err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+        }
+
+        return AdminVehicles.getAllVehicles(result);
+    });
+}
+
+AdminVehicles.add = function(vehicle, result) {
+    
+    let query = `INSERT INTO vehicles SET ?`;
+
+    sql.query(query, vehicle ,(err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+        }
+
+        return AdminVehicles.getAllVehicles(result);
+    });
+}
+
+AdminVehicles.update = function(vehicle, result) {
+    
+    let query = `UPDATE vehicles SET ? WHERE id = ?`;
+
+    sql.query(query, [vehicle, vehicle.id] ,(err, res) => {
         if (err) {
             console.log("error: ", err);
             result(err, null);
