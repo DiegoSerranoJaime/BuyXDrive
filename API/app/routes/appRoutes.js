dca647e1d5f58d6ba217605ad083eda3ce0795c5
DESKTOP-RA6IOE8\Usuario <diegoserranojaime2001@gmail.com>
2021-06-08 21:25:58 +0200

    [HOTFIX] [API] Restauracion validacion comentario y eliminación validación de emailWithExceptions

--- API/app/routes/appRoutes.js
+++ API/app/routes/appRoutes.js
@@ -407,8 +407,8 @@ module.exports = function(app) {
     app.route('/api/email/:email')
         .get(Users.emailValidation);
 
-    app.route('/api/email/:email/except/:exception')
-        .get(Users.emailExceptionValidation);
+    app.route('/api/comments/exist/product/:productId')
+        .get(verifyToken, Comments.commentAlreadyExistValidation);
 
     app.route('/api/products/:id/amount/:amount')
         .get(Products.amountValidation);
