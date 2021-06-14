0cbd07101ea14a9d65db589f884b5cb7e700abfd
DESKTOP-RA6IOE8\Usuario <diegoserranojaime2001@gmail.com>
2021-06-14 20:25:51 +0200

    [HOTFIX] [API] Archivo de configuración global agregado

--- API/app/model/db.js
+++ API/app/model/db.js
@@ -1,13 +1,17 @@
 'use strict';
 
 const mysql = require('mysql');
+const fs = require('fs');
+
+let rawData = fs.readFileSync('./assets/config/config.json');
+let data = JSON.parse(rawData);
 
 // Conexión local a la base de datos
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'buyxdrive'
+    host: data.mysql.host,
+    user: data.mysql.user,
+    password: data.mysql.password,
+    database: data.mysql.database
 });
 
 // En el caso de que haya un error en la conexión lanzamos el error
--- API/server.js
+++ API/server.js
@@ -3,13 +3,16 @@ const bodyParser = require('body-parser');
 const routes = require('./app/routes/appRoutes');
 const cors = require('cors');
 const app = express();
-// const ws = require('websocket').client;
+const fs = require('fs');
+
+let rawData = fs.readFileSync('./assets/config/config.json');
+let data = JSON.parse(rawData);
 
 // CORS
 app.use(cors());
 
 // Puerto que se va a utilizar
-port = process.env.PORT || 3000;
+port = process.env.PORT || data.express;
 
 // Arrancamos la api escuchando a dicho puerto
 app.listen(port, () => {
