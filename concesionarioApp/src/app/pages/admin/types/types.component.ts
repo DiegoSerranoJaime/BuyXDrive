a5b0b97bc1aa9991a63529b983c9fc6182e35b30
DESKTOP-RA6IOE8\Usuario <diegoserranojaime2001@gmail.com>
2021-06-14 18:57:57 +0200

    [HOTFIX] [WEB] Arreglos faltas de ortografia

--- concesionarioApp/src/app/pages/admin/types/types.component.ts
+++ concesionarioApp/src/app/pages/admin/types/types.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class TypesComponent implements OnInit {
 
   public links: any[] = [
-    { name: 'Vehiculos', route: './vehicles'},
+    { name: 'Vehículos', route: './vehicles'},
     { name: 'Artículos', route: './articles'}
   ];
 
